59e79daf80f1ee18954fd2ea9c15405550a3b9ad
yana <y.chiliasova@gmail.com>
2026-05-16 12:46:08 +0300

    Модуль 2: класс Character с ветвлением warrior/mage/hunter

--- game_vasya.py
+++ game_vasya.py
@@ -35,3 +35,54 @@ class Unit(ABC):
     def calculate_defense(self) -> int:
         """Рассчитывает защиту."""
         pass
+
+class Character(Unit):
+    """
+    Класс персонажа.
+    Может быть: 'warrior', 'mage', 'hunter'.
+    """
+
+    def __init__(
+        self,
+        strength: int,
+        dexterity: int,
+        constitution: int,
+        wisdom: int,
+        intelligence: int,
+        charisma: int,
+        character_class: str
+    ) -> None:
+        super().__init__(
+            strength, dexterity, constitution,
+            wisdom, intelligence, charisma
+        )
+
+        self.character_class: str = character_class.lower()
+
+        if self.character_class not in ['warrior', 'mage', 'hunter']:
+            raise ValueError("Неверный класс персонажа")
+
+        self.max_health: int = self.calculate_max_health()
+        self.damage: int = self.calculate_damage()
+        self.defense: int = self.calculate_defense()
+
+    def calculate_max_health(self) -> int:
+        return self.constitution * 10 + self.strength // 2
+
+    def calculate_damage(self) -> int:
+        if self.character_class == 'warrior':
+            return int(self.strength * 2.2) + self.constitution // 3
+        elif self.character_class == 'mage':
+            return int(self.intelligence * 2.5) + self.wisdom // 2
+        elif self.character_class == 'hunter':
+            return int(self.dexterity * 1.9) + self.strength // 3
+        return 0
+
+    def calculate_defense(self) -> int:
+        if self.character_class == 'warrior':
+            return int(self.constitution * 1.8) + self.strength // 4
+        elif self.character_class == 'mage':
+            return int(self.wisdom * 1.3) + self.intelligence // 6
+        elif self.character_class == 'hunter':
+            return int(self.dexterity * 1.6) + self.constitution // 5
+        return 0
